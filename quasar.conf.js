a45477a842a14a59e3895036acab12f1e0158517
torss <37229901+torss@users.noreply.github.com>
2018-08-07 04:52:21 +0200

    Add publicPath to quasar.conf.js

--- quasar.conf.js
+++ quasar.conf.js
@@ -19,6 +19,7 @@ module.exports = function (ctx) {
     build: {
       scopeHoisting: true,
       vueRouterMode: 'history',
+      publicPath: 'ultil-rfc',
       // vueCompiler: true,
       // gzip: true,
       // analyze: true,
